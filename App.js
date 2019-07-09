1e2e3f4a9e4a5990e11f7149fd8cb62ac252a3b5
adibest <ahmadjayadibya@gmail.com>
2019-07-09 20:49:35 +0700

    flex 1

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+
+  render() {
+
+    return (
+        <View style={styles.container}>
+          <View style={styles.v1}></View>
+          <View style={styles.v2}></View>
+          <View style={styles.v3}>
+            <View style={styles.v4}></View>
+            <View style={styles.v5}></View>
+          </View>
+        </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  v1: {
+    flex: 1,
+    backgroundColor: 'red',
+    width: '100%',
+  },
+  v2: {
+    flex: 1,
+    backgroundColor: 'green',
+    width: '100%',
+  },
+  v3: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    width: '100%',
+  },
+  v4: {
+    flex: 1,
+    backgroundColor: 'aqua',
+    width: '100%',
+  },
+  v5: {
+    flex: 1,
+    backgroundColor: 'lime',
+    width: '100%',
+  },
+})
