5e5bed5b4e0638f2b24a4c5975ef517b9f7d7096
adibest <ahmadjayadibya@gmail.com>
2019-07-10 20:44:55 +0700

    button

--- App.js
+++ App.js
@@ -1,104 +1,34 @@
-import React, { Component } from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
-
-import Article from './Article.js';
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
 
 export default class App extends Component {
 
+  state = {
+    content: 'Adib'
+  };
 
+  touchHandle() {
+    this.setState({
+      content: 'Bida'
+    });
+  }
+ 
   render() {
-
-    return (
-        <ScrollView>
-
-          <View style={styles.container}>
-
-            <Article 
-              title="judul 1"
-              content="ini content"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-            <Article 
-              title="judul 2 "
-              content="ini content ini isinya"
-            />
-
-          </View>
-
-        </ScrollView>
+    return(
+        <View 
+          style={{ 
+            flex:1, 
+            justifyContent: 'center',
+            alignItems: 'center', 
+          }}
+        >
+          <Text> {this.state.content} </Text>
+          <Button
+            title="Ubah State"
+            onPress={ () => this.touchHandle() }
+          />
+        </View>
       );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+}
