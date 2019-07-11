0cf3ca511d77293cc9095b4111cc8a30cf4de1a2
adibest <ahmadjayadibya@gmail.com>
2019-07-11 20:28:34 +0700

    peulangan map

--- App.js
+++ App.js
@@ -1,96 +1,43 @@
-import React, {Component} from 'react';
-import {View, Text, Button, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback, TouchableWithoutFeedback} from 'react-native';
+import React from 'react';
+import {View, Text, ScrollView} from 'react-native';
 
-export default class App extends Component {
+let todos = [
+  {
+    title: 'Mau Tidur',
+    desc: 'Ngantuk lor ngantuk lor ngantuk lor'
+  },
+  {
+    title: 'Mau Makan',
+    desc: 'Luwe lor luwe lor luwe lor'
+  },
+  {
+    title: 'Mau Mau',
+    desc: 'Mua lor Mua lor Mua lor'
+  },
+];
 
-  state = {
-    content: 1,
-    baca: false,
-  };
+export default class App extends React.Component {
 
-  bacaHandle() {
-    this.setState({
-      baca: !this.state.baca
-    });
-  }
-
-  touchHandle(arg) {
-    let result = this.state.content + arg;
-
-    this.setState({
-      content: result,
-    });
-  }
- 
   render() {
     return(
-        <View 
-          style={{ 
-            flex:1, 
+        <View
+          style={{
+            flex: 1,
             justifyContent: 'center',
-            alignItems: 'center', 
+            alignItems: 'center',
           }}
         >
-          <Text> {this.state.content} </Text>
-
-          {/*this is button biasa*/}
-          <Button
-            title="Tambah 1"
-            onPress={ () => this.touchHandle(1) }
-          />
-
-        {/*this is touchable opacity*/}
-          <TouchableOpacity
-            onPress={() => this.touchHandle(2)}
-          >
-            <View>
-              <Text>Saya text 1</Text>
-              <Text>Saya text 2</Text>
-              <Text>Saya text 3</Text>
-            </View>
-          </TouchableOpacity>
-
-        {/*this is touchable highlight*/}
-          <TouchableHighlight
-            onPress={() => this.touchHandle(2)}
-          >
-            <View>
-              <Text>Saya text 1</Text>
-              <Text>Saya text 2</Text>
-              <Text>Saya text 3</Text>
-            </View>
-          </TouchableHighlight>
-
-        {/*this is touchable natuve feedback*/}
-          <TouchableNativeFeedback
-            onPress={() => this.touchHandle(2)}
-          >
-            <View style={{
-              backgroundColor: 'yellow',
-              borderRadius: 10,
-              width: 100,
-              elevation: 5,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-              <Text>Saya text 1</Text>
-              <Text>Saya text 2</Text>
-              <Text>Saya text 3</Text>
-            </View>
-          </TouchableNativeFeedback>
-
-        {/*this is touchable without feedback*/}
-          <TouchableWithoutFeedback
-            onPress={() => this.bacaHandle()}
-            style={{marginTop: 15}}
-          >
-            <View style={{backgroundColor: this.state.baca ? 'lime' : 'aqua'}}>
-              <Text>Bismillah</Text>
-            </View>
-          </TouchableWithoutFeedback>
           
-        </View>
-      );
-  }
+          {todos.map( (todo, index) => (
+            
+            <View key={index}>
+              <Text>{index}</Text>
+              <Text>{todo.title}</Text>
+              <Text>{todo.desc}</Text>
+            </View>
+          ) )}
 
+        </View>
+    );
+  }
 }
